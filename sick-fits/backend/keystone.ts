c7810a968bd41c39599511c842e18e498979071f
Dayane Floriani Antunes <dflorianiantunes@zendesk.com>
2021-11-03 18:03:20 +0000

    Add ProductImage data type

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -6,7 +6,8 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
-import { Product } from './schemas/Products';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -44,6 +45,7 @@ export default withAuth(
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // Show UI only for people who pass this test
