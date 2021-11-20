c6637e0f8ae0dd101dc9a98932ea3c93eacb7f61
Dayane Floriani Antunes <dflorianiantunes@zendesk.com>
2021-11-20 22:37:51 +0000

    Reset password with token

---
+++ sick-fits/frontend/components/Reset.js
@@ -0,0 +1,75 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import Error from './ErrorMessage';
+
+const RESET_MUTATION = gql`
+  mutation RESET_MUTATION(
+    $email: String!
+    $password: String!
+    $token: String!
+  ) {
+    redeemUserPasswordResetToken(
+      email: $email
+      token: $token
+      password: $password
+    ) {
+      code
+      message
+    }
+  }
+`;
+
+export default function Reset({ token }) {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+    token,
+  });
+  const [reset, { data, loading, error }] = useMutation(RESET_MUTATION, {
+    variables: inputs,
+  });
+  const successfulError = data?.redeemUserPasswordResetToken?.code
+    ? data.redeemUserPasswordResetToken
+    : undefined;
+  async function handleSubmit(e) {
+    e.preventDefault(); // stop form from submitting
+    await reset().catch(console.error);
+    resetForm();
+  }
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Reset your password</h2>
+      <Error error={error || successfulError} />
+      <fieldset>
+        {data?.redeemUserPasswordResetToken === null && (
+          <p>Success! You can now sign in.</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Reset password</button>
+      </fieldset>
+    </Form>
+  );
+}
